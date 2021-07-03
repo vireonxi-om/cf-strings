df96039c4ce618b9cb32d1b5c2bf29444063fd8b
rheallyc <vireonxi@gmail.com>
2021-07-03 11:07:24 +0000

    Solve 1552A: Subsequence Permutation

---
+++ solutions/1552A_Subsequence_Permutation.cpp
@@ -0,0 +1,53 @@
+// Problem: Subsequence Permutation
+// URL: https://codeforces.com/problemset/problem/1552/A
+// Rating: 800
+// Tags: sortings, strings
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 650;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+
+int n;
+char s[50], temp[50];
+
+void solve() {
+    scanf("%d%s", &n, s+1);
+    repn(i, 1, n) temp[i]=s[i];
+    sort(temp+1, temp+n+1);
+    int cnt=0;
+    repn(i, 1, n) if(s[i]!=temp[i]) cnt++;
+    printf("%d\n", cnt);
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) solve();
+    return 0;
+}
+
