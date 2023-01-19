189fc2eeb89789a51fa8d611fc4ae1207c2f7122
rheallyc <vireonxi@gmail.com>
2023-01-19 04:59:29 +0000

    Solve 1758A: SSeeeeiinngg DDoouubbllee

---
+++ solutions/1758A_SSeeeeiinngg_DDoouubbllee.cpp
@@ -0,0 +1,52 @@
+// Problem: SSeeeeiinngg DDoouubbllee
+// URL: https://codeforces.com/problemset/problem/1758/A
+// Rating: 800
+// Tags: constructive algorithms, strings
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 210000;
+const int M = 110000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+void solve() {
+	string s;
+	cin >> s; 
+	cout << s;
+	per(i, 0, s.length()) cout << s[i];
+	cout << "\n";
+}
+
+int main() {
+    IO;
+    int t;
+	cin >> t;
+	repn(i, 1, t) solve(); 
+    return 0;
+}
+
+
+
