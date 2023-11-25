abbd498cc486d6c21a9b69518907e910ef004eff
rheallyc <vireonxi@gmail.com>
2023-11-25 23:51:19 +0000

    Solve 1844D: Row Major

---
+++ solutions/1844D_Row_Major.cpp
@@ -0,0 +1,61 @@
+// Problem: Row Major
+// URL: https://codeforces.com/problemset/problem/1844/D
+// Rating: 1400
+// Tags: constructive algorithms, greedy, math, number theory, strings
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k << 1
+#define RC k << 1 | 1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+template<typename T> void down(T &x, T y) { if (x > y) x = y; }
+
+const int N = 110000;
+const int M = 610000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const LL INF = (LL)1e12 + 5;
+const double eps = 1e-15;
+const double pi = acos(-1.0);
+
+int n;
+void solve() {
+	cin >> n;
+	int ans = 0;
+	repn(i, 1, 26) {
+		if (n % i == 0) continue;
+		ans = i; break;
+	}
+	int cur = 0;
+	repn(i, 1, n) {
+		char c = 'a' + cur;
+		cur ++; cur %= ans;
+		cout << c;
+	}
+	cout << "\n";
+}
+
+int main() {
+	IO;
+	int T;
+	cin >> T;
+	repn(i, 1, T) solve();
+	return 0;
+	
+}
