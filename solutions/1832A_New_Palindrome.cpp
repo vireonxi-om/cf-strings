3a2da7847bfc4a577698a43996ada73bdf49e45e
rheallyc <vireonxi@gmail.com>
2025-07-05 23:51:57 +0000

    Solve 1832A: New Palindrome

---
+++ solutions/1832A_New_Palindrome.cpp
@@ -0,0 +1,54 @@
+// Problem: New Palindrome
+// URL: https://codeforces.com/problemset/problem/1832/A
+// Rating: 800
+// Tags: strings
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+ 
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+typedef long double LD;
+ 
+const int N = 51000;
+const int M = 1100000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+ 
+string s;
+ 
+void solve() {
+	cin >> s;
+	int n = s.length();
+	bool yes = false;
+	repn(i, 1, n / 2 - 1) {
+		if (s[i] != s[0]) yes = true;
+	}
+	if (yes) cout << "YES\n";
+	else cout << "NO\n";
+}
+int main() {
+	int t;
+	cin >> t;
+	repn(i, 1, t) solve();
+	return 0;
+}
+
